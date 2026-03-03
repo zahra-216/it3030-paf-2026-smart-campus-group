1dc28a3fb222f2e6ef7ddf1af7a983302fd8c364
zahra-216 <zahrahasan2163@gmail.com>
2026-03-03 16:49:37 +0530

    Add all entity models

---
+++ backend/src/main/java/com/paf/unidesk/model/TicketAttachment.java
@@ -0,0 +1,36 @@
+package com.paf.unidesk.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ticket_attachments")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class TicketAttachment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "ticket_id", nullable = false)
+    private Ticket ticket;
+
+    @Column(nullable = false)
+    private String fileUrl;
+}
