68d3861a7a1f8bc19552bbb9702413858b1b7a00
Zahra <zahrahasan2163@gmail.com>
2026-04-06 23:54:17 +0530

    Add BaseEntity with JPA auditing for createdAt and updatedAt

--- backend/src/main/java/com/paf/unidesk/UnideskApplication.java
+++ backend/src/main/java/com/paf/unidesk/UnideskApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class UnideskApplication {
 
 	public static void main(String[] args) {
